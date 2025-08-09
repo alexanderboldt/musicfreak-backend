cd936c947e390a64b4216cd9fc36c507de61e97b
Alexander Boldt <boldt.alexander@outlook.com>
2025-08-09 18:33:37 +0200

    adjusted integration-tests with test-profile

--- src/test/kotlin/com/alex/musicfreak/controller/BaseControllerTest.kt
+++ src/test/kotlin/com/alex/musicfreak/controller/BaseControllerTest.kt
@@ -17,31 +17,9 @@ import jakarta.inject.Inject
 import jakarta.transaction.Transactional
 import org.apache.http.HttpStatus
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
-import org.testcontainers.containers.PostgreSQLContainer
-import org.testcontainers.junit.jupiter.Container
 
 open class BaseControllerTest {
-
-    companion object {
-        @Container
-        private val postgres = PostgreSQLContainer<Nothing>("postgres:17.4").apply {
-            withDatabaseName("testDb")
-            withUsername("test")
-            withPassword("test")
-        }
-
-        @JvmStatic
-        @BeforeAll
-        fun beforeAllBase() {
-            postgres.start()
-
-            System.setProperty("quarkus.datasource.jdbc.url", postgres.jdbcUrl)
-            System.setProperty("quarkus.datasource.username", postgres.username)
-            System.setProperty("quarkus.datasource.password", postgres.password)
-        }
-    }
 
     @BeforeEach
     @Transactional
