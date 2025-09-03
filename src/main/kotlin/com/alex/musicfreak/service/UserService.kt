0f23f0a494775ed343639a47f0fd9af36468a595
Alexander Boldt <boldt.alexander@outlook.com>
2025-09-03 10:35:26 +0200

    created user service to retrieve the id; close #14

---
+++ src/main/kotlin/com/alex/musicfreak/service/UserService.kt
@@ -0,0 +1,11 @@
+package com.alex.musicfreak.service
+
+import io.quarkus.security.identity.SecurityIdentity
+import jakarta.enterprise.context.ApplicationScoped
+
+@ApplicationScoped
+class UserService(private val securityIdentity: SecurityIdentity) {
+
+    val userId: String
+        get() = securityIdentity.principal.name
+}
